19d838120f4b0137c4690f7a46370c39fa66e56a
Dawid <dawid.duraj@deepstrm.com>
2020-12-25 15:39:07 +0100

    Added player rotation
    player rotates between 2 points with the ability to lock the rotation.

--- Chicken Fly/Assets/PlayerLauncher.cs
+++ Chicken Fly/Assets/PlayerLauncher.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class PlayerLauncher : MonoBehaviour
 {
     public float LaunchVelocity = 5f;
-    
+    public float RotationSpeed = 2f;
+
+
     Text speedtext;
+    bool rotationLock = false;
     bool launched = false;
     Rigidbody2D rb;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
@@ -31,13 +34,24 @@ public class PlayerLauncher : MonoBehaviour
         {
             return;
         }
+        
+        if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) && !rotationLock)
+        {
+            rotationLock = true;
+            return;
+        }
+        if (!rotationLock)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, (35f * Mathf.Sin(Time.time * RotationSpeed)) + 45);
+        }
 
-        if (Input.GetMouseButtonDown(0) && !launched)
+        if (Input.GetMouseButtonDown(0) && !launched && rotationLock)
         {
             rb.WakeUp();
             rb.velocity = rb.transform.right * LaunchVelocity;
             launched = true;
         }
+
         speedtext.text = "Speed: " + Mathf.RoundToInt(rb.velocity.magnitude).ToString() + " m/s";
 
     }

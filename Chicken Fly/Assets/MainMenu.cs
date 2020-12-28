b9a0dff4fbaf48b6932b3fcf4f9fd40f75deae80
Dawid <dawid.duraj@deepstrm.com>
2020-12-28 18:51:00 +0100

    Added Menu GUI
    Added GUI with title and Buttons

---
+++ Chicken Fly/Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    Canvas menu;
+    Canvas Shop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void StartButtonPress()
+    {
+
+    }
+
+    void ShopButtonPress()
+    {
+
+    }
+}
